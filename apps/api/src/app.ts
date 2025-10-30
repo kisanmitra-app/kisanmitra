aa0525d21d3cc7f1b6e7a198308929b6c0b2d776
susmitadas22 <susmitadas9639@gmail.com>
2025-10-30 16:35:56 +0530

    feat: added rate limit

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { rateLimiter } from "hono-rate-limiter";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import { loggingMiddleware } from "./middlewares";
 import { routes } from "./routes";
+import { getConnInfo } from "@hono/node-server/conninfo";
 
 /**
  * @description Hono app instance
@@ -23,6 +25,24 @@ app.use(
 
 // logger
 app.use("*", loggingMiddleware);
+
+// Apply the rate limiting middleware to all requests.
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 200, // Limit each IP to 200 requests per `window` (here, per 15 minutes).
+    //  dev
+    // windowMs: 5 * 1000, // 5 seconds
+    // limit: 5, // Limit each IP to 5 requests per `window` (here, per 6 seconds).
+    message: { message: "Too many requests, please try again later." },
+    keyGenerator: (c) => {
+      const cfIp = c.req.raw.headers.get("CF-Connecting-IP");
+      const info = getConnInfo(c); // info is `ConnInfo`
+      const key = cfIp || info.remote.address || crypto.randomUUID();
+      return key;
+    },
+  })
+);
 
 // register routes
 app.route("/", routes);
